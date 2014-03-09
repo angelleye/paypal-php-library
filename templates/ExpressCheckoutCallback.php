4bc98f5cf3bcc6a9066f39cd5a0e51e84fa2e1a6
Andrew Angell <andrew+git@angelleye.com>
2014-03-09 11:36:56 -0500

    Moves / renames EC callback template.

--- templates/ExpressCheckoutCallback.php
+++ templates/ExpressCheckoutCallback.php
@@ -45,7 +45,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 require_once('../includes/config.php');
-require_once('PayPal/PayPal.php');
+require_once('../includes/PayPal/PayPal.php');
 
 // Configure PayPal object
 $paypal_config = array('Sandbox' => $sandbox);
