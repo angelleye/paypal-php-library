ccc3f64d850ae26239d4e27c81b9959621fb361c
Andrew Angell <service@angelleye.com>
2014-11-05 12:05:56 -0600

    Removes unused check for ACK

--- samples/GetBalance.php
+++ samples/GetBalance.php
@@ -23,11 +23,6 @@ $PayPalRequestData = array('GBFields'=>$GBFields);
 // Pass data into class for processing with PayPal and load the response array into $PayPalResult
 $PayPalResult = $PayPal->GetBalance($PayPalRequestData);
 
-if($PayPal->APICallSuccessful($PayPalResult['ACK']))
-{
-		
-}
-
 // Write the contents of the response array to the screen for demo purposes.
 echo '<pre />';
 print_r($PayPalResult);
