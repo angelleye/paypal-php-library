c39ddab6386622050825f739111ec1fca6b217ba
tejas <tejasmht88@gmail.com>
2017-01-02 12:35:24 +0530

    Webprofile partial update and full update API. ref #21

--- src/angelleye/PayPal/rest/paymentexperience/PaymentExperianceAPI.php
+++ src/angelleye/PayPal/rest/paymentexperience/PaymentExperianceAPI.php
@@ -2,8 +2,9 @@
 namespace angelleye\PayPal\rest\paymentexperience;
 
 use \PayPal\Api\FlowConfig;
-use \PayPal\Api\Presentation;
 use \PayPal\Api\InputFields;
+use \PayPal\Api\Patch;
+use \PayPal\Api\Presentation;
 use \PayPal\Api\WebProfile;
 
 class PaymentExperianceAPI {
@@ -76,6 +77,67 @@ class PaymentExperianceAPI {
         } catch (\PayPal\Exception\PayPalConnectionException $ex) {
             return $ex->getData();
         }
+    }
+    
+    public function partially_update_webprofile($patchArray,$profileID){
+
+        $webProfile = new WebProfile();
+        $webProfile->setId($profileID);
+        
+        $patches=array();
+        
+        foreach ($patchArray as $patch){
+           if($patch['Op']=='remove'){
+              unset($patch['Value']);
+           }
+            $patchOperation = new Patch();
+            $this->setArrayToMethods($patch, $patchOperation);
+            array_push($patches, $patchOperation);
+        }              
+        try {
+            // Execute the partial update, to carry out these two operations on a given web profile object
+            if ($webProfile->partial_update($patches, $this->_api_context)) {
+                $webProfile = WebProfile::get($webProfile->getId(), $this->_api_context);
+                return $webProfile;
+            }
+        } catch (\PayPal\Exception\PayPalConnectionException $ex) {
+            return $ex->getData();
+        }
+    }
+
+    public function update_web_profile($requestData,$profileID){
+        
+        try {            
+            // #### Payment Web experience profile resource
+            $webProfile = $this->get_web_profile($profileID);
+            
+            if($this->checkEmptyObject($requestData['FlowConfig'])){
+                $flowConfig = $webProfile->getFlowConfig();
+                $this->setArrayToMethods(array_filter($requestData['FlowConfig']), $flowConfig);
+                $webProfile->setFlowConfig($flowConfig);
+            }
+
+            if($this->checkEmptyObject($requestData['presentation'])){
+                $presentation = $webProfile->getPresentation();
+                $this->setArrayToMethods(array_filter($requestData['presentation']), $presentation);
+                $webProfile->setPresentation($presentation);
+            }
+
+            if($this->checkEmptyObject($requestData['InputFields'])){
+                $inputFields = $webProfile->getInputFields();
+                $this->setArrayToMethods(array_filter($requestData['InputFields']), $inputFields);
+                $webProfile->setInputFields($inputFields);
+            }
+            if($this->checkEmptyObject($requestData['WebProfile'])){
+                $this->setArrayToMethods(array_filter($requestData['WebProfile']), $webProfile);
+            }
+            
+            $webProfile->update($this->_api_context);
+            $updatedWebProfile = WebProfile::get($profileID, $this->_api_context);
+            return $updatedWebProfile;
+        } catch (\PayPal\Exception\PayPalConnectionException $ex) {
+            return $ex->getData();
+        }        
     }
 
     public function checkEmptyObject($array){
