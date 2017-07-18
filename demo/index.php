f26ad0e65ab31e86f2b847385f8750cd30a3ed7e
WORKSTATION\andre <andrew@angelleye.com>
2017-07-18 07:08:03 -0500

    Removes duplicate demo kit, ref #76

--- demo/index.php
+++ demo/index.php
@@ -137,25 +137,13 @@ The sample code provided by the download buttons is pulled straight from this si
         <div class="col-md-4">
             <div class="thumbnail"> <img alt="Express Checkout Recurring Payment" src="assets/images/express-checkout-recurring-payments.jpg">
             <div class="caption">
-              <h3> Express Checkout   </h3>
-              <h4> Recurring Payments </h4>
-              <p> Learn how to implement Express Checkout - Recurring Payments.  This includes the option for embedded payments. </p>
-              <p> <a class="btn btn-primary" href="express-checkout-recurring-payments/">Demo</a> <a class="btn btn-info" href="download/paypal-php-express-checkout-digital-goods-demo.zip">Download Sample Code</a> </p>
+              <h3>Express Checkout</h3>
+              <h4>Recurring Payments</h4>
+              <p>Learn how to implement Express Checkout - Recurring Payments.  This includes the option for embedded payments.</p>
+              <p><a class="btn btn-primary" href="express-checkout-recurring-payments/">Demo</a> <a class="btn btn-info" href="download/paypal-php-express-checkout-digital-goods-demo.zip">Download Sample Code</a> </p>
             </div>
           </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-md-4">
-            <div class="thumbnail"> <img alt="Do Express Checkout Recurring Payment" src="assets/images/do-express-checkout-recurring-payments.jpg">
-            <div class="caption">
-              <h3> Do Express Checkout Payment </h3>
-              <h4> Recurring Payments </h4>
-              <p> Learn how to implement Do Express Checkout Recurring Payment".  This includes the option for embedded payments. </p>
-              <p> <a class="btn btn-primary" href="do-express-checkout-recurring-payments/">Demo</a> <a class="btn btn-info" href="download/paypal-php-express-checkout-digital-goods-demo.zip">Download Sample Code</a> </p>
-            </div>
-          </div>
-        </div>  
       </div>
     </div>
   </div>
