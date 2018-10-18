ae60906f8a2a3140b2894824a63ba3d155007e3c
tejas <tejasmht88@gmail.com>
2018-10-18 19:16:06 +0530

    PPCL-146 , added comments and description for CreditCardAPI.php

--- src/angelleye/PayPal/rest/vault/CreditCardAPI.php
+++ src/angelleye/PayPal/rest/vault/CreditCardAPI.php
@@ -1,16 +1,74 @@
-<?php namespace angelleye\PayPal\rest\vault;
+<?php 
+
+namespace angelleye\PayPal\rest\vault;
+
+
+/**
+ *	An open source PHP library written to easily work with PayPal's API's
+ *	
+ *	Email:  service@angelleye.com
+ *  Facebook: angelleyeconsulting
+ *  Twitter: angelleye
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * @package			paypal-php-library
+ * @author			Andrew Angell <service@angelleye.com>
+ * @link			https://github.com/angelleye/paypal-php-library/
+ * @website			http://www.angelleye.com
+ * @support         http://www.angelleye.com/product/premium-support/
+ * @version			v2.0.4
+ * @filesource
+*/
 
 use \angelleye\PayPal\RestClass;
 use PayPal\Api\CreditCard;
 
+/**
+ * CreditCardAPI.
+ * This class is responsible for CreditCard APIs & bridge class between the REST API class and Angelleye PayPal Library.
+ *
+ * @package 		paypal-php-library
+ * @author			Andrew Angell <service@angelleye.com>
+ */
 class CreditCardAPI extends RestClass {
 
+    /**
+     * Private vairable to fetch and return @PayPal\Rest\ApiContext object.
+     *
+     * @var \PayPal\Rest\ApiContext $_api_context 
+     */
     private $_api_context;
+
+    /**
+	 * Constructor
+	 *
+	 * @access	public
+	 * @param	mixed[]	$configArray Array structure providing config data
+	 * @return	void
+	 */
     public function __construct($configArray) {        
         parent::__construct($configArray);
         $this->_api_context = $this->get_api_context();
     }    
     
+    /**
+     * Stores credit card details in the PayPal vault.
+     *
+     * @param array $requestData
+     * @return array|object
+     */
     public function StoreCreditCard($requestData){
         $creditCard = new CreditCard();
         
@@ -41,7 +99,13 @@ class CreditCardAPI extends RestClass {
            return $this->createErrorResponse($ex);
         }        
     }
-        
+    
+    /**
+     * Lists vaulted credit cards.
+     *
+     * @param array $requestData
+     * @return array|object
+     */
     public function listAllCards($requestData) {   
         $creditCard = new \PayPal\Api\CreditCard();
         try {            
@@ -58,6 +122,12 @@ class CreditCardAPI extends RestClass {
         }
     }
     
+    /**
+     * Shows details for a vaulted credit card, by ID.
+     *
+     * @param array $requestData
+     * @return array|object
+     */
     public function showByID($requestData){        
         $creditCard = new CreditCard();
             try {
@@ -73,6 +143,12 @@ class CreditCardAPI extends RestClass {
             }                    
     }
     
+    /**
+     * Deletes a vaulted credit card, by ID.
+     *
+     * @param array $requestData
+     * @return array|object
+     */
     public function deleteByID($requestData){        
         $creditCard = new \PayPal\Api\CreditCard();        
         try {
@@ -87,7 +163,14 @@ class CreditCardAPI extends RestClass {
             return $this->createErrorResponse($ex);
         }
     }
-        
+       
+    /**
+     * Updates information for a vaulted credit card, by ID. 
+     *
+     * @param array $requestData
+     * @param string $credit_card_id
+     * @return array|object
+     */
     public function UpdateCreditCard($requestData,$credit_card_id){
             $creditCard = new \PayPal\Api\CreditCard();    
             $pathRequest = new \PayPal\Api\PatchRequest();
