64100dfaaf0932fde31f086651b7150ca11ae5c1
tejas <tejasmht88@gmail.com>
2018-10-22 12:25:22 +0530

    PPCL-147 , added code for logs in webhook api.

--- samples/rest/notifications/CreateWebhook.php
+++ samples/rest/notifications/CreateWebhook.php
@@ -5,7 +5,10 @@ require_once('../../../includes/config.php');
 
 $configArray = array(
                 'ClientID' => $rest_client_id,
-                'ClientSecret' => $rest_client_secret
+                'ClientSecret' => $rest_client_secret,
+                'LogResults' => $log_results, 
+                'LogPath' => $log_path,
+                'LogLevel' => $log_level  
                 );
 
 $PayPal = new \angelleye\PayPal\rest\notifications\NotificationsAPI($configArray);
