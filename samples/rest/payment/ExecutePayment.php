8f7dd9d3f96f80e96bee3c0105a41fc75616ee09
tejas <tejasmht88@gmail.com>
2018-11-23 16:28:42 +0530

    PPCL-180 , added execute payment function.

--- samples/rest/payment/ExecutePayment.php
+++ samples/rest/payment/ExecutePayment.php
@@ -14,73 +14,48 @@ require_once('../../../includes/config.php');
 // information by passing in one or more transactions.
 // API used: POST '/v1/payments/payment/<payment-id>/execute'.
 
-use PayPal\Api\Amount;
-use PayPal\Api\Details;
-use PayPal\Api\ExecutePayment;
-use PayPal\Api\Payment;
-use PayPal\Api\PaymentExecution;
-use PayPal\Api\Transaction;
+$configArray = array(
+    'ClientID' => $rest_client_id,
+    'ClientSecret' => $rest_client_secret,
+    'LogResults' => $log_results,
+    'LogPath' => $log_path,
+    'LogLevel' => $log_level
+);
 
-$_api_context = new \PayPal\Rest\ApiContext(
-                new \PayPal\Auth\OAuthTokenCredential($rest_client_id,$rest_client_secret)
-            );
-echo "<pre>";
+$PayPal = new angelleye\PayPal\rest\payments\PaymentAPI($configArray);
+
 // ### Approval Status
 // Determine if the user approved the payment or not
 if (isset($_GET['success']) && $_GET['success'] == 'true') {
 
-    // Get the payment Object by passing paymentId
-    // payment id was previously stored in session in
-    // CreatePaymentUsingPayPal.php
     $paymentId = $_GET['paymentId'];
-    $payment = Payment::get($paymentId, $_api_context);
-
-    // ### Payment Execute
-    // PaymentExecution object includes information necessary
-    // to execute a PayPal account payment.
-    // The payer_id is added to the request query parameters
-    // when the user is redirected from paypal back to your site
-    $execution = new PaymentExecution();
-    $execution->setPayerId($_GET['PayerID']);
+    $payer_id = $_GET['PayerID'];
 
     // ### Optional Changes to Amount
     // If you wish to update the amount that you wish to charge the customer,
     // based on the shipping address or any other reason, you could
     // do that by passing the transaction object with just `amount` field in it.
-    // Here is the example on how we changed the shipping to $1 more than before.
-    /*$transaction = new Transaction();
-    $amount = new Amount();
-    $details = new Details();
 
-    $details->setShipping(2.2)
-        ->setTax(1.3)
-        ->setSubtotal(17.50);
+    $details = array(
+        'Shipping' => '2.2',
+        'Tax' => '1.3',
+        'HandlingFee' => '',
+        'ShippingDiscount' => '',
+        'Insurance' => '',
+        'GiftWrap' => '',
+        'Fee' => '',
+        'Subtotal' => '17.50'
+    );
 
-    $amount->setCurrency('USD');
-    $amount->setTotal(21);
-    $amount->setDetails($details);
-    $transaction->setAmount($amount);
 
-    // Add the above transaction object inside our Execution object.
-    $execution->addTransaction($transaction);*/
+    $amount = array(
+        'Currency' => 'USD',
+        'Total' => '21',
+        'Details' => $details
+    );
 
-    try {
-        // Execute the payment
-        $result = $payment->execute($execution, $_api_context);
-        echo "<h1>You Payement state : {$result->state} </h1>";
-        print_r($result->toArray());
-        try {
-            $payment = Payment::get($paymentId, $_api_context);
-            echo "<h1>Your Payment object</h1>";
-            print_r($payment->toArray());
-        } catch (\PayPal\Exception\PayPalConnectionException  $ex) {
-            print_r($ex->getData());
-            exit;
-        }
-    } catch (\PayPal\Exception\PayPalConnectionException  $ex) {
-        print_r($ex->getData());
-        exit;
-    }
+    $PayPal->execute_payment($paymentId,$payer_id,$amount);
+
 } else {
     echo "User Cancelled the Approval";
     exit;
