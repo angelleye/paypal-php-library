a2efbe1c18135afe72b99a2699a601ea55ba9173
tejas <tejasmht88@gmail.com>
2019-05-14 11:03:57 +0530

    PPL-161 , Adjustments to the code for notifications listing,.

--- samples/rest/notifications/CreateWebhook.php
+++ samples/rest/notifications/CreateWebhook.php
@@ -13,19 +13,10 @@ $configArray = array(
 
 $PayPal = new \angelleye\PayPal\rest\notifications\NotificationsAPI($configArray);
 
-$event_types = array();
-
-$event_type_1 = array(
-    'Name' => 'VAULT.CREDIT-CARD.CREATED',           // The unique event name. You can fine list of webhooks name in PayPal_Webhooks.txt
+$event_types = array(
+    'VAULT.CREDIT-CARD.CREATED', // The unique event name. You can fine list of webhooks name in PayPal_Webhooks.txt
+    'VAULT.CREDIT-CARD.DELETED'  // The unique event name. You can fine list of webhooks name in PayPal_Webhooks.txt
 );
-
-array_push($event_types, $event_type_1);
-
-$event_type_2 = array(
-    'Name' => 'VAULT.CREDIT-CARD.DELETED',           // The unique event name. You can fine list of webhooks name in PayPal_Webhooks.txt
-);
-
-array_push($event_types, $event_type_2);
 
 $url = "https://requestb.in/10ujt3c1?uniqid=" . uniqid();          // The URL that is configured to listen on localhost for incoming POST notification messages that contain event information.
 
