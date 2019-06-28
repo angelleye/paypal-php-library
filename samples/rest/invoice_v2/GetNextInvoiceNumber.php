c7e28cb557c7ce45148f6e2b0db95f7664fcef24
tejas <tejasmht88@gmail.com>
2019-06-28 11:07:22 +0530

    PPL-170 , added next invoice number api for version two.

---
+++ samples/rest/invoice_v2/GetNextInvoiceNumber.php
@@ -0,0 +1,22 @@
+<?php
+
+use angelleye\PayPal\rest\invoice\InvoiceAPIv2;
+
+
+// Include required library files.
+require_once('../../../autoload.php');
+require_once('../../../includes/config.php');
+
+$configArray = array(
+    'Sandbox' => $sandbox,
+    'ClientID' => $rest_client_id,
+    'ClientSecret' => $rest_client_secret,
+    'LogResults' => $log_results, 
+    'LogPath' => $log_path,
+    'LogLevel' => $log_level  
+);
+$PayPal = new \angelleye\PayPal\rest\invoice\InvoiceAPIv2($configArray);
+
+$returnArray = $PayPal->GetNextInvoiceNumber();
+echo "<pre>";
+print_r($returnArray);
